9861a4ed7a77ca66611e19024b28110cf6216cac
OscarMateosRamos <dsi85944@educastur.es>
2025-02-18 12:47:57 +0100

    Clase de repositorio para el cliente creada

---
+++ tarea7DWESOscar/src/main/java/com/oscar/vivero/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.oscar.vivero.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.oscar.vivero.modelo.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+}
