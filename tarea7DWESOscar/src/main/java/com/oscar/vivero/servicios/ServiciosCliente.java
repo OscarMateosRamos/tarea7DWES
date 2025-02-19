dadef6cb2a4f309d5d39f9dafdc27a2c9d37df4d
OscarMateosRamos <dsi85944@educastur.es>
2025-02-19 11:11:27 +0100

    Cambios realizados en el servicio del cliente

--- tarea7DWESOscar/src/main/java/com/oscar/vivero/servicios/ServiciosCliente.java
+++ tarea7DWESOscar/src/main/java/com/oscar/vivero/servicios/ServiciosCliente.java
@@ -1,14 +1,58 @@
 package com.oscar.vivero.servicios;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.oscar.vivero.modelo.Cliente;
+import com.oscar.vivero.modelo.Persona;
 import com.oscar.vivero.repositories.ClienteRepository;
 
 @Service
 public class ServiciosCliente {
 
 	@Autowired
-	ClienteRepository credencialrepo;
-	
+	ClienteRepository clienterepo;
+
+	public boolean validarCliente(String nombre, String email) {
+
+		if (nombre.length() > 255) {
+			System.out.println("Nombre invalido");
+			return false;
+		}
+
+		if (email.length() > 255) {
+			System.out.println("Email invalido");
+			return false;
+		}
+
+		String patron = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+		Pattern pattern = Pattern.compile(patron);
+
+		Matcher matcher = pattern.matcher(email);
+		if (!matcher.matches()) {
+			System.out.println(" Formato de email invalido..." + email);
+			return false;
+		}
+
+		List<Cliente> clientes = clienterepo.findAll();
+		for (Cliente c : clientes) {
+			if (c.getEmail().equals(email)) {
+				System.out.println(c.getEmail());
+				System.out.println("El email ya existe...." + email);
+				return false;
+			}
+		}
+
+		return true;
+
+	}
+
+	public void insertarPlanta(Cliente c) {
+		clienterepo.saveAndFlush(c);
+	}
+
 }
